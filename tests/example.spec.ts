dcb75ba69f5a9bfe581c9117dc9a4d66d99a40cf
nazarit1 <nazarit1@iastate.edu>
2025-11-07 15:03:41 -0600

    better tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,47 @@
 import { test, expect } from '@playwright/test';
 
+let email = "nazarit1@iastate.edu"
+let password = "FakePassword!"
+
+
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://hivemind-qgfariece-hiveminds-projects-62c372da.vercel.app/auth/login');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Supabase/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+/* 
+  Test when a user has an account
+*/
 
+test('login test success', async ({ page }) => {
+  await page.goto('https://hivemind-qgfariece-hiveminds-projects-62c372da.vercel.app/auth/login');
+  
+  // enter email and password
+  await page.getByLabel('Email').fill(email);
+  await page.getByLabel('Password').fill(password);
+  
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('button', { name: 'Login' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByText("This is a protected page that you can only see as an authenticated user")).toBeVisible();
+});
+
+/* 
+  Test when a user does not have an account
+*/
+test('login test failure', async ({ page }) => {
+  await page.goto('https://hivemind-qgfariece-hiveminds-projects-62c372da.vercel.app/auth/login');
+  
+  // enter email and password
+  await page.getByLabel('Email').fill('noaccount@gmail.com');
+  await page.getByLabel('Password').fill('whoami');
+  
+  // Click the get started link.
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByText("Invalid login credentials")).toBeVisible();
 });
