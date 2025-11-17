6ac968ea876e495443675fb2f70576809940e1e0
Chris Moseley <chrisrmoseley1@gmail.com>
2025-11-17 16:51:02 -0600

    commented out/removed boiler plate components

--- app/page.tsx
+++ app/page.tsx
@@ -17,22 +17,25 @@ export default function Home() {
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>Next.js Supabase Starter</Link>
               <div className="flex items-center gap-2">
-                <DeployButton />
+                {/* <DeployButton /> */}
               </div>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
+
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
+          {/* <Hero /> */}
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            {/* <h2 className="font-medium text-xl mb-4">Next steps</h2> */}
+            {/*
+              {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+            */}
           </main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
+          {/* <p>
             Powered by{" "}
             <a
               href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
@@ -42,11 +45,10 @@ export default function Home() {
             >
               Supabase
             </a>
-          </p>
+          </p> */}
           <ThemeSwitcher />
         </footer>
       </div>
     </main>
   );
 }
-// initial change for autodeployment
