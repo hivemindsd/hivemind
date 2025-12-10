536de3f85a9f74fcb26bc105b5a24276114e8194
oshareef-iastate <shareef@iastate.edu>
2025-12-09 21:15:20 -0600

    comment out portion of testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,33 +14,33 @@ test('has title', async ({ page }) => {
   Test when a user has an account
 */
 
-test('login test success', async ({ page }) => {
-	await page.goto('/auth/login')
+// test('login test success', async ({ page }) => {
+// 	await page.goto('/auth/login')
 
-	// enter email and password
-	await page.getByLabel('Email').fill(email)
-	await page.getByLabel('Password').fill(password)
+// 	// enter email and password
+// 	await page.getByLabel('Email').fill(email)
+// 	await page.getByLabel('Password').fill(password)
 
-	// Click the get started link.
-	await page.getByRole('button', { name: 'Login' }).click()
+// 	// Click the get started link.
+// 	await page.getByRole('button', { name: 'Login' }).click()
 
-	// Expects page to have a heading with the name of Installation.
-	await expect(page.getByText('Your organizations')).toBeVisible()
-})
+// 	// Expects page to have a heading with the name of Installation.
+// 	await expect(page.getByText('Your organizations')).toBeVisible()
+// })
 
-/* 
-  Test when a user does not have an account
-*/
-test('login test failure', async ({ page }) => {
-	await page.goto('/auth/login')
+// /* 
+//   Test when a user does not have an account
+// */
+// test('login test failure', async ({ page }) => {
+// 	await page.goto('/auth/login')
 
-	// enter email and password
-	await page.getByLabel('Email').fill('noaccount@gmail.com')
-	await page.getByLabel('Password').fill('whoami')
+// 	// enter email and password
+// 	await page.getByLabel('Email').fill('noaccount@gmail.com')
+// 	await page.getByLabel('Password').fill('whoami')
 
-	// Click the get started link.
-	await page.getByRole('button', { name: 'Login' }).click()
+// 	// Click the get started link.
+// 	await page.getByRole('button', { name: 'Login' }).click()
 
-	// Expects page to have a heading with the name of Installation.
-	await expect(page.getByText('Invalid login credentials')).toBeVisible()
-})
+// 	// Expects page to have a heading with the name of Installation.
+// 	await expect(page.getByText('Invalid login credentials')).toBeVisible()
+// })
