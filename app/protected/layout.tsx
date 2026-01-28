34f3d9a9dfb1337b0c365bec3893a0e0f3c39aa8
nazarit1 <nazarit1@iastate.edu>
2026-01-27 19:47:51 -0600

    no unused vars

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -3,28 +3,34 @@ import { AuthButton } from '@/components/account/auth-button'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { hasEnvVars } from '@/lib/utils'
 import Link from 'next/link'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className='min-h-screen flex flex-col items-center'>
-			<div className='flex-1 w-full flex flex-col gap-2'>
-				<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
-					<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-						<div className='flex gap-5 items-center'>
-							<Link href={'/'} className='font-dancing-script text-4xl font-bold'>
-								Hivemind
-							</Link>
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarTrigger />
+			<main className='flex w-full flex-col items-center'>
+				<div className='flex-1 w-full flex flex-col gap-2'>
+					<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
+						<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
+							<div className='flex gap-5 items-center'>
+								<Link href={'/'} className='font-dancing-script text-4xl font-bold'>
+									Hivemind
+								</Link>
+							</div>
+							{!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
 						</div>
-						{!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-					</div>
-				</nav>
-				<div className='flex-1 flex flex-col gap-2 p-5'>{children}</div>
+					</nav>
+					<div className='flex-1 flex flex-col gap-2 p-5'>{children}</div>
 
-				<footer className='w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-4 py-4'>
-					<p>Powered by sdmay26-03</p>
-					<ThemeSwitcher />
-				</footer>
-			</div>
-		</main>
+					<footer className='w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-4 py-4'>
+						<p>Powered by sdmay26-03</p>
+						<ThemeSwitcher />
+					</footer>
+				</div>
+			</main>
+		</SidebarProvider>
 	)
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,14 +1,98 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from '@/components/ui/sidebar'
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupLabel,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuItem,
+	SidebarMenuButton
+} from '@/components/ui/sidebar'
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+
+import { createClient } from '@/lib/supabase/server'
+import { LogoutButton } from '@/components/account/logout-button'
+
+import Link from 'next/link'
+import { EnvVarWarning } from '@/components/env-var-warning'
+import { hasEnvVars } from '@/lib/utils'
+
+async function Username() {
+	const supabase = await createClient()
+
+	// You can also use getUser() which will be slower.
+	const { data } = await supabase.auth.getClaims()
+
+	const user = data?.claims
+
+	return user ? (
+		<div className='flex flex-col items-center gap-4'>
+			<span className='hidden md:block'>{user.email}</span>
+		</div>
+	) : (
+		<>ERROR</>
+	)
+}
 
 export function AppSidebar() {
+	const items = [
+		{
+			title: 'Home',
+			url: '#'
+		},
+		{
+			title: 'Inbox',
+			url: '#'
+		},
+		{
+			title: 'Settings',
+			url: '#'
+		}
+	]
+
 	return (
-		<Sidebar>
-			<SidebarHeader />
+		<Sidebar variant='floating'>
 			<SidebarContent>
-				<SidebarGroup />
-				<SidebarGroup />
+				<SidebarGroup>
+					<SidebarGroupLabel>Hivemind</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a href={item.url}>
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
 			</SidebarContent>
-			<SidebarFooter />
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton>{!hasEnvVars ? <EnvVarWarning /> : <Username />}</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent side='top' className='w-[--radix-popper-anchor-width]'>
+								<DropdownMenuItem>
+									<Link className='mx-auto' href='#'>
+										Account
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<LogoutButton />
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
 		</Sidebar>
 	)
 }
